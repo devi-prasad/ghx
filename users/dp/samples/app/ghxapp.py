b31b28f7d98c6497575389c102c49a8589f8b538
dprasad <dprasadm@gmail.com>
2015-12-23 14:36:20 +0530

    creating a sample program to access github repositories using http requests.

---
+++ users/dp/samples/app/ghxapp.py
@@ -0,0 +1,6 @@
+import ghx
+from ghx import repo
+
+if __name__ == "__main__":
+  print("scripting app!")
+
---
+++ users/dp/samples/pyinit.sh
@@ -0,0 +1,6 @@
+export GHXHOME=`pwd`
+export PYTHONPATH=$GHXHOME:$PYTHONPATH
+export https_proxy=172.16.19.10:80
+export http_proxy=172.16.19.10:80
+
+#export PYTHONHOME=/usr/
